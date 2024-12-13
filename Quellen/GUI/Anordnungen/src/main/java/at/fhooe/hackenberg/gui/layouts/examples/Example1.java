efa78b3468d1ec5456f0a15332f1dc7c40dba11c
Dr. Georg Hackenberg <ghackenberg@gmail.com>
2024-12-13 08:22:47 +0100

    document code

--- Quellen/GUI/Anordnungen/src/main/java/at/fhooe/hackenberg/gui/layouts/examples/Example1.java
+++ Quellen/GUI/Anordnungen/src/main/java/at/fhooe/hackenberg/gui/layouts/examples/Example1.java
@@ -9,11 +9,16 @@ import javafx.scene.layout.HBox;
 public class Example1 {
 
 	public static Node create() {
+		
+		// Inhalte des Layouts erstellen
+		
 		HBox center = new HBox(new Label("Center"));
 		HBox top = new HBox(new Label("Top"));
 		HBox left = new HBox(new Label("Left"));
 		HBox right = new HBox(new Label("Right"));
 		HBox bottom = new HBox(new Label("Bottom"));
+		
+		// Innere Randabstände setzen
 		
 		center.setPadding(new Insets(10));
 		top.setPadding(new Insets(10));
@@ -21,11 +26,15 @@ public class Example1 {
 		right.setPadding(new Insets(10));
 		bottom.setPadding(new Insets(10));
 		
+		// Hintergrundfarben setzen
+		
 		center.setStyle("-fx-background-color: orange;");
 		top.setStyle("-fx-background-color: orange;");
 		left.setStyle("-fx-background-color: orange;");
 		right.setStyle("-fx-background-color: orange;");
 		bottom.setStyle("-fx-background-color: orange;");
+		
+		// Äußere Randabstände setzen
 		
 		BorderPane.setMargin(center, new Insets(5));
 		BorderPane.setMargin(top, new Insets(5));
@@ -33,12 +42,17 @@ public class Example1 {
 		BorderPane.setMargin(right, new Insets(5));
 		BorderPane.setMargin(bottom, new Insets(5));
 		
+		// Layout selbst erstellen
+		
 		BorderPane pane = new BorderPane(center, top, right, bottom, left);
 		
 		pane.setPadding(new Insets(5));
 		pane.setStyle("-fx-background-color: lightgray");
 		
+		// Layout inklusive Inhalte zurückgeben
+		
 		return pane;
+		
 	}
 	
 }
--- Quellen/GUI/Anordnungen/src/main/java/at/fhooe/hackenberg/gui/layouts/examples/Example2.java
+++ Quellen/GUI/Anordnungen/src/main/java/at/fhooe/hackenberg/gui/layouts/examples/Example2.java
@@ -12,40 +12,57 @@ import javafx.scene.layout.RowConstraints;
 public class Example2 {
 
 	public static Node create() {
+		
+		// Inhalte des Layouts erstellen
+		
 		HBox top = new HBox(new Label("Top"));
 		HBox left = new HBox(new Label("Left"));
 		HBox right = new HBox(new Label("Right"));
 		HBox bottom = new HBox(new Label("Bottom"));
+		
+		// Innere Randabstände setzen
 		
 		top.setPadding(new Insets(10));
 		left.setPadding(new Insets(10));
 		right.setPadding(new Insets(10));
 		bottom.setPadding(new Insets(10));
 		
+		// Hintergrundfarben setzen
+		
 		top.setStyle("-fx-background-color: orange;");
 		left.setStyle("-fx-background-color: orange;");
 		right.setStyle("-fx-background-color: orange;");
 		bottom.setStyle("-fx-background-color: orange;");
 		
+		// Layout selbst erstellen
+		
 		GridPane pane = new GridPane(10, 10);
 		
 		pane.setPadding(new Insets(10));
+		pane.setStyle("-fx-background-color: lightgray");
+		
+		// Spalten konfigurieren
 		
 		pane.getColumnConstraints().add(createColumnConstraints(100/3.));
 		pane.getColumnConstraints().add(createColumnConstraints(100/3.));
 		pane.getColumnConstraints().add(createColumnConstraints(100/3.));
 		
+		// Zeilen konfigurieren
+		
 		pane.getRowConstraints().add(createRowConstraints(100/2.));
 		pane.getRowConstraints().add(createRowConstraints(100/2.));
+		
+		// Inhalte hinzufügen
 		
 		pane.add(top, 0, 0, 2, 1);
 		pane.add(left, 2, 0, 1, 1);
 		pane.add(right, 0, 1, 1, 1);
 		pane.add(bottom, 1, 1, 2, 1);
-		
-		pane.setStyle("-fx-background-color: lightgray");
+
+		// Layout inklusive Inhalte zurückgeben
 		
 		return pane;
+		
 	}
 	
 	private static ColumnConstraints createColumnConstraints(double percent) {
